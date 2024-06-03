7e564bf1212a9022591295069e68b6959621e8af
nparker7317 <nparker7317@student.pps.net>
2024-06-03 11:40:22 -0700

    I finished the trash can and added a vortex spiral animation.

---
+++ Assets/TrashCan.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashCan : MonoBehaviour
+{
+    public void Start(){
+        GetComponent<TriggerZone>().OnEnterEvent.AddListener(InsideTrash);
+    }
+   public void InsideTrash(GameObject go){
+    go.SetActive(false);
+   }
+}
